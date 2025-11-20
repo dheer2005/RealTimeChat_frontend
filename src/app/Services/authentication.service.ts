3f4fcd49ddccd13f97eaa42c305ed8405b5d0eb1
Dhirendra <dhirendranyadav@gmail.com>
2025-11-20 11:18:19 +0530

    ChangesUrls

--- src/app/Services/authentication.service.ts
+++ src/app/Services/authentication.service.ts
@@ -17,15 +17,15 @@ export class AuthenticationService {
   private tokenKey = 'jwt';
   
   public UserName:any;
-  // seenUrl: any = "https://realtime001.bsite.net/api/Seen/messages/" 
-  // chatUrl: any = "https://realtime001.bsite.net/api/ChatHub/"
-  // baseUrl: any = "https://realtime001.bsite.net/api/Authentication/"
-  // mediaUrl: any = "https://realtime001.bsite.net/api/Media/"
+  seenUrl: any = "https://realtime001.bsite.net/api/Seen/messages/" 
+  chatUrl: any = "https://realtime001.bsite.net/api/ChatHub/"
+  baseUrl: any = "https://realtime001.bsite.net/api/Authentication/"
+  mediaUrl: any = "https://realtime001.bsite.net/api/Media/"
 
-  seenUrl: any = "https://localhost:7180/api/Seen/messages/" 
-  chatUrl: any = "https://localhost:7180/api/ChatHub/"
-  baseUrl: any = "https://localhost:7180/api/Authentication/"
-  mediaUrl: any = "https://localhost:7180/api/Media/"
+  // seenUrl: any = "https://localhost:7180/api/Seen/messages/" 
+  // chatUrl: any = "https://localhost:7180/api/ChatHub/"
+  // baseUrl: any = "https://localhost:7180/api/Authentication/"
+  // mediaUrl: any = "https://localhost:7180/api/Media/"
 
   // seenUrl: any = "https://10.0.0.43:5000/api/Seen/messages/" 
   // chatUrl: any = "https://10.0.0.43:500/api/ChatHub/"
